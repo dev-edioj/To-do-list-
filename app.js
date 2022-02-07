5a34688f72d2eb22f7181f33157b8aa35fe01247
edio <dev.edioj@gmail.com>
2022-02-07 07:49:22 -0300

    routes and add nodemon

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const express = require('express');
+
+
+const app = express();
+
+//criar uma rota
+app.get('/', (req, res) => {
+    res.send('<h1>Minha lista de tarefas</h1>');
+})
+
+app.listen(3000, () => {
+    console.log('Servidor iniciado')
+})

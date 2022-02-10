1e08d0064a00c7a83859ce03a68e7ee914d6493e
edio <dev.edioj@gmail.com>
2022-02-09 22:16:18 -0300

    routes

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -6,5 +6,26 @@ router.get('/', (res, req) => {
     console.log('Olá');
     res.send();
 })
+router.post('/', (res, req) => {
+    console.log(req.body);
+    res.status(200).json(req.body);
+});
+
+router.get('/:id', (req, res) => {
+    console.log(req.params.id);
+    res.send(`ID: ${req.params.id}`)
+})
+
+router.put('/:id', (req, res) => {
+    console.log(req.params.id);
+    res.send(`PUT ID: ${req.params.id}`)
+})
+
+router.delete('/:id', (req, res) => {
+    console.log(req.params.id);
+    res.send(`DELETE ID: ${req.params.id}`)
+})
+
+router.delete()
 
 module.exports = router;
